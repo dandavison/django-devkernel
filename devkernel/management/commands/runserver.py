ceae213d5952b22d304fd08bfe799626f15bf55d
Dan Davison <dandavison7@gmail.com>
2019-06-02 23:46:46 +0300

    Add optimistic reload argument

--- devkernel/management/commands/runserver.py
+++ devkernel/management/commands/runserver.py
@@ -1,3 +1,4 @@
+import optimistic_reload.django
 from django.core.management.commands import runserver
 
 import devkernel.autoreload
@@ -11,15 +12,29 @@ class DevkernelRunserverMixin:
             action='store_true', dest='start_kernel', default=False,
             help='Start a Jupyter Python kernel in the server process.',
         )
+        parser.add_argument(
+            '--optimistic-reload',
+            action='store_true', dest='use_optimistic_reloader', default=False,
+            help=('When code is changed on disk, attempt to reload the minimal set of required '
+                  'modules, without restarting the server.'),
+        )
         super().add_arguments(parser)
 
     def run(self, **options):
+        if options['use_optimistic_reloader']:
+            devkernel.autoreload.file_changed.connect(
+                optimistic_reload.django.file_changed_signal_handler
+            )
         start_kernel = options.pop('start_kernel')
         if not start_kernel:
             super().run(**options)
         else:
             assert options['use_reloader'], '--kernel cannot be used with --noreload'
-            devkernel.autoreload.run_with_reloader(self.inner_run, **options)
+            if options['use_optimistic_reloader']:
+                with optimistic_reload.django.apply_patches():
+                    devkernel.autoreload.run_with_reloader(self.inner_run, **options)
+            else:
+                devkernel.autoreload.run_with_reloader(self.inner_run, **options)
 
 
 class Command(DevkernelRunserverMixin, runserver.Command):
